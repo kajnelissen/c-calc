6d623067251fb3c5c724ad6e7f02416781dbce96
kj91 <kaj@kj91.nl>
2014-01-10 08:22:08 -0500

    Main en Calculate

---
+++ calculator.c
@@ -0,0 +1,20 @@
+#include <calculator.h>
+
+int main(int argc, char **argv)
+{
+	// check if we have the necessary argument
+	if (argc == 2)
+	{
+		
+	}
+}
+
+void calculate(char[] string) 
+{
+	// process input
+
+	// choose function to execute
+
+	// print return value
+}
+
