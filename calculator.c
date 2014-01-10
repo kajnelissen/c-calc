7be80c31eb1bdcd9931859a5f9862d810ce40703
Kaj Nelissen <kaj@kj91.nl>
2014-01-10 14:24:56 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,5 +1,14 @@
 #include < calculator.h>
 
+int main(int argc, char **argv)
+{
+        // check if we have the necessary argument
+        if (argc == 2)
+        {
+                
+        }
+}
+
 int add(int getal1, int getal2)
 {
 	return getal1 + getal2;
@@ -18,4 +27,13 @@ int mul(int getal1, int getal2)
 int div(int getal1, int getal2)
 {
 	return getal1 / getal2;
+}
+
+void calculate(char[] string)
+{
+        // process input
+
+        // choose function to execute
+
+        // print return value
 }
