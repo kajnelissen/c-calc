0b08e38653057fd94a2cfe800189d8f6b711fa55
gerard24 <gerard.24@live.nl>
2014-01-10 14:20:16 +0100

    calculator.c gemaakt

---
+++ calculator.c
@@ -0,0 +1,21 @@
+#include < calculator.h>
+
+int add(int getal1, int getal2)
+{
+	return getal1 + getal2;
+}
+
+int sub(int getal1, int getal2)
+{
+	return getal1 - getal2;
+}
+
+int mul(int getal1, int getal2)
+{
+	return getal1 * getal2;
+}
+
+int div(int getal1, int getal2)
+{
+	return getal1 / getal2;
+}
