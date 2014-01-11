5e5ec9161bcc8f27e4243bc420b79a924213e3f2
gerard24 <gerard.24@live.nl>
2014-01-11 14:54:01 +0100

    Update testAdd.sh

--- testAdd.sh
+++ testAdd.sh
@@ -8,28 +8,28 @@ source calculator.c
 #test3: 4+-2
 #test4: -3+-1
 
-RESULT = calculate("sum(1, 5)")
+RESULT = calculate("add(1, 5)")
 if [ "$RESULT" -eq 6 ]; then
 	echo &'Result is: ${RESULT}. Correct!/n'
 else
 	echo &'Result is: ${RESULT}. Incorrect!/n'
 fi
 
-RESULT = calculate("sum(-4, 2)")
+RESULT = calculate("add(-4, 2)")
 if [ "$RESULT" -eq -2 ]; then
 	echo &'Result is: ${RESULT}. Correct!/n'
 else
 	echo &'Result is: ${RESULT}. Incorrect!/n'
 fi
 
-RESULT = calculate("sum(4, -2)")
+RESULT = calculate("add(4, -2)")
 if [ "$RESULT" -eq 2 ]; then
 	echo &'Result is: ${RESULT}. Correct!/n'
 else
 	echo &'Result is: ${RESULT}. Incorrect!/n'
 fi
 
-RESULT = calculate("sum(-3, -1)")
+RESULT = calculate("add(-3, -1)")
 if [ "$RESULT" -eq -4 ]; then
 	echo &'Result is: ${RESULT}. Correct!/n'
 else
