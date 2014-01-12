10b5a0074047be4d7bc69cc12ad12e335ea88436
gerard24 <gerard.24@live.nl>
2014-01-12 15:44:53 +0100

    message

--- testAdd.sh
+++ testAdd.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
 
-include "calculator.c"
-source calculator.c
- 
 #test1: 1+5
 #test2: -4+2
 #test3: 4+-2
 #test4: -3+-1
 
-RESULT = calculate("add(1, 5)")
+RESULT = cp_binary "ADD 1, 5"
 if [ "$RESULT" -eq 6 ]; then
 	echo &'Result is: ${RESULT}. Correct!/n'
 else
 	echo &'Result is: ${RESULT}. Incorrect!/n'
 fi
 
-RESULT = calculate("add(-4, 2)")
+RESULT = cp_binary "ADD -4, 2"
 if [ "$RESULT" -eq -2 ]; then
 	echo &'Result is: ${RESULT}. Correct!/n'
 else
 	echo &'Result is: ${RESULT}. Incorrect!/n'
 fi
 
-RESULT = calculate("add(4, -2)")
+RESULT = cp_binary "ADD 4, -2"
 if [ "$RESULT" -eq 2 ]; then
 	echo &'Result is: ${RESULT}. Correct!/n'
 else
 	echo &'Result is: ${RESULT}. Incorrect!/n'
 fi
 
-RESULT = calculate("add(-3, -1)")
+RESULT = cp_binary "ADD -3, -1"
 if [ "$RESULT" -eq -4 ]; then
 	echo &'Result is: ${RESULT}. Correct!/n'
 else
--- testSub.sh
+++ testSub.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
-
-include "calculator.c"
-source calculator.c
  
 #test1: 5-1
 #test2: 2--4
 #test3: -2-4
 #test4: -1--3
 
-RESULT = calculate("sub(5, 1)")
+RESULT = cp_binary "SUB 5, 1"
 if [ "$RESULT" -eq 4 ]; then
         echo &'Result is: ${RESULT}. Correct!/n'
 else
         echo &'Result is: ${RESULT}. Incorrect!/n'
 fi
 
-RESULT = calculate("sub(2, -4)")
+RESULT = cp_binary "SUB 2, -4"
 if [ "$RESULT" -eq 6 ]; then
         echo &'Result is: ${RESULT}. Correct!/n'
 else
         echo &'Result is: ${RESULT}. Incorrect!/n'
 fi
 
-RESULT = calculate("sub(-2, 4)")
+RESULT = cp_binary "SUB -2, 4"
 if [ "$RESULT" -eq -6 ]; then
         echo &'Result is: ${RESULT}. Correct!/n'
 else
         echo &'Result is: ${RESULT}. Incorrect!/n'
 fi
 
-RESULT = calculate("sub(-1, -3)")
+RESULT = cp_binary "SUB -1, -3"
 if [ "$RESULT" -eq 2 ]; then
         echo &'Result is: ${RESULT}. Correct!/n'
 else
