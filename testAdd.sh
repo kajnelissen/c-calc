0d2972a514998ab38cb5821144df20ccc3ad69da
gerard24 <gerard.24@live.nl>
2014-01-15 16:40:28 +0100

    Update testAdd.sh

--- testAdd.sh
+++ testAdd.sh
@@ -5,31 +5,36 @@
 #test3: 4+-2
 #test4: -3+-1
 
-RESULT = cp_binary "ADD 1, 5"
-if [ "$RESULT" -eq 6 ]; then
-	echo &'Result is: ${RESULT}. Correct!/n'
-else
-	echo &'Result is: ${RESULT}. Incorrect!/n'
-fi
+source color-echo.sh
 
-RESULT = cp_binary "ADD -4, 2"
-if [ "$RESULT" -eq -2 ]; then
-	echo &'Result is: ${RESULT}. Correct!/n'
-else
-	echo &'Result is: ${RESULT}. Incorrect!/n'
-fi
+make cp_binary
 
-RESULT = cp_binary "ADD 4, -2"
-if [ "$RESULT" -eq 2 ]; then
-	echo &'Result is: ${RESULT}. Correct!/n'
-else
-	echo &'Result is: ${RESULT}. Incorrect!/n'
-fi
+	RESULT = $(./cp_binary "ADD 1 5")
+	if [ "${RESULT}" == "6.00000" ]; then
+		cecho "1 + 5 = ${RESULT}" $green
+	else
+		cecho "1 + 5 = ${RESULT}" $red
+	fi
+	
+	RESULT = $(./cp_binary "ADD -4 2")
+	if [ "${RESULT}" == "-2.00000" ]; then
+		cecho "-4 + 2 = ${RESULT}" $green
+	else
+		cecho "-4 + 2 = ${RESULT}" $red
+	fi
+	
+	RESULT = $(./cp_binary "ADD 4 -2")
+	if [ "${RESULT}" == "2.00000" ]; then
+		cecho "4 + -2 = ${RESULT}" $green
+	else
+		cecho "4 + -2 = ${RESULT}" $red
+	fi
+	
+	RESULT = $(./cp_binary "ADD -3 -1")
+	if [ "${RESULT}" == "-4.00000" ]; then
+		cecho "-3 + -1 = ${RESULT}" $green
+	else
+		cecho "-3 + -1 = ${RESULT}" $red
+	fi
 
-RESULT = cp_binary "ADD -3, -1"
-if [ "$RESULT" -eq -4 ]; then
-	echo &'Result is: ${RESULT}. Correct!/n'
-else
-	echo &'Result is: ${RESULT}. Incorrect!/n'
-fi
-
+make clean
